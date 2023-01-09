2686e87a210ca8633a7bc8e4314d852045b2fcbd
Trung_Hieu_Dev <dangtrunghieusg@gmail.com>
2023-01-09 09:24:13 +0700

    Lession 05: Variable

---
+++ Java_05_Variable/src/Vidu.java
@@ -0,0 +1,21 @@
+
+public class Vidu {
+	public static void main(String[] args) {
+		String tenSach = "Lap trinh Java";
+		int namXuatBan = 2021;
+		double giaTien = 15.5;
+		boolean conSach = true;
+		char maKho = 'M';
+		System.out.println("Ten sach: " + tenSach);
+		System.out.println("Nam xuat ban: " + namXuatBan);
+		System.out.println("Gia tien: " + giaTien);
+		
+		if(conSach) {
+			System.out.println("Tinh trang: con hang");
+		} else {
+			System.out.println("Tinh trang: het hang");
+		}
+		
+		System.out.println("Ma kho: " + maKho);
+	}
+}
